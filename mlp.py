57dda1344d146171b22ecff490f9069a8c6c62e0
Sahand Zarrinkoub <sahandzarrinkoub@n145-p177.eduroam.kth.se>
2018-02-18 17:43:51 +0100

    hi

--- mlp.py
+++ mlp.py
@@ -1,3 +1,22 @@
 import tensorflow as tf
+from Untitled import gen_datasets, add_noise
+
+dataset = gen_dataset()
+
+datasets["ysin_train"] = add_noise(datasets["ysin_train"])
+datasets["ysin_test"] = add_noise(datasets["ysin_test"])
+
+patterns_train = datasets["x_train"]
+targets_train = datasets["ysin_train"]
+patterns_test = datasets["x_test"]
+targets_test = datasets["ysin_test"]
+
+pattern = tf.placeholder(tf.float64)
+
+node1 = tf.Variable()
+node2 = tf.Variable()
+
+node3 = tf.Variable()
+node4 = tf.Variable()
 
 
